355cf08e37d9cc65c8b01841d248bccd5cd894b1
Ogalababa <delay7.su@gmail.com>
2022-03-11 11:34:23 +0100

    optimize page.py add intro.py add tramspeed.py

---
+++ Show/sub_pages/intro.py
@@ -0,0 +1,17 @@
+# ！/usr/bin/python3
+# coding:utf-8
+
+import streamlit as st
+
+
+def intro():
+    st.sidebar.success("Selecteer een pagina hierboven")
+
+    st.markdown(
+        """
+        ### Dit is een HTM Data-analyse project.
+
+        ##### **👈 Selecteer een pagina in de vervolgkeuzelijst aan de linkerkant** 
+        om meer analysetools te zien!
+    """
+    )
