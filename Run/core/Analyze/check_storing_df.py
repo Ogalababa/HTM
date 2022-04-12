0cbb86e0dcd7e1b17df6cb10a360a0112b1983c8
suj <delay7.su@gmail.com>
2022-04-12 09:22:27 +0200

    HTM 2.13 add check_storing_df.py

--- Run/core/Analyze/check_storing_df.py
+++ Run/core/Analyze/check_storing_df.py
@@ -17,21 +17,20 @@ def check_storing_df(df):
         len(set(df["<aanmelden> wagen"])) > 2,
         len(set(df["<wissel> op slot"])) < 2
     ]
-    if any(check_error_list):
-        return True
-    else:
-        return False
-    
+    return any(check_error_list)
+
+
 def recheck_storing(df):
     try:
         step_revert = 0
         step_list = df['step']
         step_list = [i for i in step_list if i is not None]
+        # is_storing = False
         for i in range(len(step_list)-1):
             if step_list[i+1] < step_list[i]:
-                step_revert +=1
-        return step_revert > 3
-                
+                step_revert += 1
+
+        return any([step_revert > 3, 0 in df['<wissel> ijzer'].to_list()])
     except:
         return True
 
