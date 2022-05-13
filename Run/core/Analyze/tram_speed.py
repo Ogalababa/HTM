13ad1eb965f8b97d215b6d6340f320e3f78b53b3
suj <delay7.su@gmail.com>
2022-05-13 12:29:13 +0200

    HTM 2.14.5 rebuild tram_speed

--- Run/core/Analyze/tram_speed.py
+++ Run/core/Analyze/tram_speed.py
@@ -2,10 +2,9 @@
 # coding:utf-8
 # sys
 
-import sqlalchemy
-from Run.core.ConvertData.VerSelect import get_wissel_type_nr
+from Run.core.Analyze.analyze_tool import check_bad_contact
 import pandas as pd
-from Run.core.ConvertData.ConnectDB import conn_engine
+from Run.core.Tools.VaribleTool import wissel_gerade
 
 
 def wagen_lent(wagen_nr):
@@ -25,84 +24,62 @@ def wagen_lent(wagen_nr):
     return lent
 
 
-def tram_speed_to_sql(log_db):
-    # 从log db中提取tram通过wissel的速度
+def calculation_tram_speed(dataset):
     """
-    Calculate tram speed over the wissel from database
-    :param log_db: str
-    :return: none
+    calculation the tram speed
+    :param dataset: pd.DataFrame
+    :return: pd.DataFrame
     """
+    speed_df_list = []
+    try:
+        # 筛选wagen数据
+        wagen_set = set(dataset[dataset['<hfk> schakelcriterium bezet'] == 1]['<afmelden> wagen'].tolist())
+        # 筛选wagen通过wissel数据集
+        for i in wagen_set:
+            richting = 'ontbekend'
+            hfk_df = dataset[dataset['<afmelden> wagen'] == i]
+            # 判断数据正确性
+            if check_bad_contact(hfk_df, '<hfk> schakelcriterium bezet', 'hfk', 'infra')[-1] is not True:
+                hfk_index_list = hfk_df[hfk_df['<hfk> schakelcriterium bezet'] == 1].index.tolist()
+                if len(hfk_index_list) >= 3:
+                    speed_dict = {}
+                    wissel_nr = dataset.loc[hfk_index_list[0]]['wissel nr']
+                    lijn_nr = dataset.loc[hfk_index_list[0]]['<aanmelden> lijn']
+                    service = dataset.loc[hfk_index_list[0]]['<aanmelden> service']
+                    categorie = dataset.loc[hfk_index_list[0]]['<aanmelden> categorie']
+                    wagen_nr = dataset.loc[hfk_index_list[0]]['<afmelden> wagen']
+                    # 判断行进方向
+                    if dataset.loc[hfk_index_list[0]]['<wissel> links'] == 1:
+                        richting = '<wissel> links'
+                    elif dataset.loc[hfk_index_list[0]]['<wissel> rechts'] == 1:
+                        richting = '<wissel> rechts'
+                    if richting == wissel_gerade(wissel_nr):
+                        richting = 'Recht door'
+                    else:
+                        richting = f'{richting[8:]} af'
+                    # 储存数据
+                    hfk_in = dataset.loc[hfk_index_list[0]]['date-time']
+                    hfk_out = dataset.loc[hfk_index_list[-1] + 1]['date-time']
+                    speed = round(wagen_lent(wagen_nr) / (hfk_out - hfk_in).total_seconds() * 3.6)
 
-    insp = sqlalchemy.inspect(conn_engine(log_db))
-    all_wissels = insp.get_table_names()
-    # all_wissels = [i for i in all_wissels if i in get_wissel_type_nr('denBDB3C')]
-    data_dict = {}
-    for wissel_nr in all_wissels:
-        try:
-            all_data = pd.read_sql_table(wissel_nr, conn_engine(log_db))
-            all_data['date-time'] = pd.to_datetime(all_data['date-time'])
-            afmeld = 0
-            lijn_nr = ''
-            hfk_in = 0
-            richting = '0'
-            hfk = False
-            for i in range(len(all_data)):
-                row = all_data.iloc[i]
-                if row['<input> naar gerade'] == 1:
-                    richting = 'Recht door'
-                elif row['<input> naar rechts'] == 1:
-                    richting = 'Rechts af'
-                elif row['<input> naar links'] == 1:
-                    richting = 'links af'
-
-                if row['<hfk> schakelcriterium bezet'] == 1 and \
-                        row['<hfp> schakelcriterium bezet'] == 1 and \
-                        row['<afmelden> wagen'] != 0 and \
-                        hfk is False:
-                    hfk_in = row['date-time']
-                    afmeld = row['<afmelden> wagen']
-                    lijn_nr = str(row['<aanmelden> lijn'])
-                    hfk = True
-                elif row['<hfk> schakelcriterium bezet'] == 0 and \
-                        row['<hfp> schakelcriterium bezet'] == 0 and \
-                        row['<afmelden> wagen'] != 0 and \
-                        hfk is True:
-                    hfk_out = row['date-time']
-                    hfk = False
-                    if afmeld == row['<afmelden> wagen'] and row['<aanmelden> lijn'] != 0:
-                        tram_speed = row[['<aanmelden> lijn',
-                                          '<afmelden> wagen',
-                                          '<aanmelden> categorie',
-                                          '<aanmelden> service',
-                                          'wissel nr']]
-                        tram_speed['Richting'] = richting
-                        tram_speed['hfk_in'] = hfk_in
-                        tram_speed['hfk_uit'] = hfk_out
-                        tram_speed['snelheid km/h'] = round(wagen_lent(row['<afmelden> wagen']) /
-                                                            (hfk_out - hfk_in).total_seconds() * 3.6)
-                        tram_speed_df = pd.DataFrame(tram_speed.to_dict(), index=[i])
-
-                        if lijn_nr in data_dict.keys():
-
-                            data_dict[lijn_nr] = pd.concat([data_dict.get(lijn_nr), tram_speed_df])
-
-                        else:
-                            data_dict[lijn_nr] = tram_speed_df
-
+                    if speed < 50:
+                        speed_dict['<aanmelden> lijn'] = [lijn_nr]
+                        speed_dict['<afmelden> wagen'] = [wagen_nr]
+                        speed_dict['<aanmelden> categorie'] = [categorie]
+                        speed_dict['<aanmelden> service'] = [service]
+                        speed_dict['wissel nr'] = [wissel_nr]
+                        speed_dict['Richting'] = [richting]
+                        speed_dict['hfk_in'] = [hfk_in]
+                        speed_dict['hfk_uit'] = [hfk_out]
+                        speed_dict['snelheid km/h'] = [speed]
+                        # 转换成data frame
+                        speed_df_list.append(pd.DataFrame(speed_dict))
                     else:
                         pass
                 else:
                     pass
-        except (ValueError, TypeError, KeyError) as err:
-            print(f'{wissel_nr}:{err}')
-            pass
-
-    for key in data_dict.keys():
-        try:
-            data_dict.get(key).to_sql(key,
-                                      conn_engine(log_db,
-                                                  path='snelheid'),
-                                      index=False,
-                                      if_exists='replace')
-        except (ValueError, TypeError, KeyError):
-            pass
+            else:
+                pass
+    except:
+        pass
+    return speed_df_list
--- Run/core/Integration/DataCalculator.py
+++ Run/core/Integration/DataCalculator.py
@@ -2,15 +2,13 @@
 # coding:utf-8
 # sys
 import pandas as pd
-from multiprocessing import Pool
 
-from Run.core.Analyze.check_storing_df import check_storing_df, recheck_storing, define_storing
+from Run.core.Analyze.check_storing_df import check_storing_df, define_storing
+from Run.core.Analyze.tram_speed import calculation_tram_speed
 from Run.core.Analyze.wissel_schakel import wissel_schakel
 from Run.core.Analyze.wissel_vrij_list import wissel_cycle_list
-from Run.core.Analyze.wissel_storing import wissel_storing
 from Run.core.Integration.DataInitialization import get_alldata_from_db
 from Run.core.Integration.DataInitialization import save_to_sql
-from Run.core.Tools.VaribleTool import wagen_length
 
 
 class Calculator:
@@ -33,69 +31,34 @@ class Calculator:
         Calculate tram speed while tram passing the wissel
         :return: dict
         """
-        data_dict = {}
-        for key, value in self.db_dict.items():
+        speed_dict = {}
+        for key, values in self.db_dict.items():
+
+            # set to time format
+            values['date-time'] = pd.to_datetime(values['date-time'])
+            # check cycles
             try:
-                value['date-time'] = pd.to_datetime(value['date-time'])
-                afmeld = 0
-                lijn_nr = ''
-                hfk_in = 0
-                richting = 'ontbekend'
-                hfk = False
-                for i in range(len(value)):
-                    row = value.iloc[i]
-                    if row['<input> naar gerade'] == 1:
-                        richting = 'Recht door'
-                    elif row['<input> naar rechts'] == 1:
-                        richting = 'Rechts af'
-                    elif row['<input> naar links'] == 1:
-                        richting = 'links af'
-
-                    if row['<hfk> schakelcriterium bezet'] == 1 and \
-                            row['<hfp> schakelcriterium bezet'] == 1 and \
-                            row['<afmelden> wagen'] != 0 and \
-                            hfk is False:
-
-                        hfk_in = row['date-time']
-                        afmeld = row['<afmelden> wagen']
-                        lijn_nr = str(row['<aanmelden> lijn'])
-                        hfk = True
-
-                    elif row['<hfk> schakelcriterium bezet'] == 0 and \
-                            row['<hfp> schakelcriterium bezet'] == 0 and \
-                            row['<afmelden> wagen'] != 0 and \
-                            hfk is True:
-
-                        hfk_out = row['date-time']
-                        hfk = False
-
-                        if afmeld == row['<afmelden> wagen'] and row['<aanmelden> lijn'] != 0:
-                            tram_speed = row[[
-                                '<aanmelden> lijn',
-                                '<afmelden> wagen',
-                                '<aanmelden> categorie',
-                                '<aanmelden> service',
-                                'wissel nr'
-                            ]]
-                            tram_speed['Richting'] = richting
-                            tram_speed['hfk_in'] = hfk_in
-                            tram_speed['hfk_uit'] = hfk_out
-                            tram_speed['snelheid km/h'] = round(wagen_length(row['<afmelden> wagen']) /
-                                                                (hfk_out - hfk_in).total_seconds() * 3.6)
-                            tram_speed_df = pd.DataFrame(tram_speed.to_dict(), index=[i])
-
-                            if lijn_nr in data_dict.keys():
-                                data_dict[lijn_nr] = pd.concat([data_dict.get(lijn_nr), tram_speed_df])
-
-                            else:
-                                data_dict[lijn_nr] = tram_speed_df
-                        else:
-                            pass
-                    else:
+                index_list = wissel_cycle_list(values)
+                speed_df_list = []
+                for i in range(len(index_list) - 1):
+                    cycle_df = values[index_list[i]:index_list[i + 1]]
+                    speed_df = calculation_tram_speed(cycle_df)
+                    if len(speed_df) == 0:
                         pass
-            except (ValueError, TypeError, KeyError, ZeroDivisionError) as err:
+                    elif len(speed_df) == 1:
+                        speed_df_list.append(speed_df[0])
+                    else:
+                        speed_df_list.append(pd.concat(speed_df))
+                if len(speed_df_list) == 0:
+                    pass
+                elif len(speed_df_list) == 1:
+                    speed_dict[key] = speed_df_list[0]
+                else:
+                    speed_dict[key] = pd.concat(speed_df_list)
+            except:
                 pass
-        save_to_sql(self.db_name, data_dict, 'snelheid')
+
+        save_to_sql(self.db_name, speed_dict, 'snelheid')
 
     def C_wissel_schakel(self):
         """
@@ -159,4 +122,3 @@ class Calculator:
                 all_storing_dict[str(x).zfill(3)] = i
                 x += 1
             save_to_sql(self.db_name, all_storing_dict, 'all_storing')
-             
